3628fc767f27e2993a100a9567bfa48fcd4de9e8
agambondan <agamgans89@gmail.com>
2020-09-23 15:42:50 +0700

    Updates

---
+++ precache-manifest.242a9618bc7e132d4ead180efd93e560.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d43f6743f69878a1f6e335e4e49106c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329e7c3b13a4d49e44aa",
+    "url": "/static/css/main.b8e4e575.chunk.css"
+  },
+  {
+    "revision": "e8e467571e3201389ebb",
+    "url": "/static/js/2.84891ec8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.84891ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329e7c3b13a4d49e44aa",
+    "url": "/static/js/main.3d26c136.chunk.js"
+  },
+  {
+    "revision": "70f2f3e96580038b90f3",
+    "url": "/static/js/runtime-main.5d0a31e7.js"
+  },
+  {
+    "revision": "91604f256b4f15032a258091cda48afe",
+    "url": "/static/media/Portfolio.91604f25.png"
+  },
+  {
+    "revision": "c73eb1b497f85cf5f22e6892b0a0dd2f",
+    "url": "/static/media/pp.c73eb1b4.jpg"
+  }
+]);
